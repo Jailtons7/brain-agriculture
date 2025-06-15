b3938aa79732fad1429a0db3b91281c14cb23f18
Jailton Silva <jss1@ifal.edu.br>
2025-06-15 12:59:37 -0300

    feat: add agriculture_api app

---
+++ src/agriculture_api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Farmer(models.Model):
+    name = models.CharField("Nome", max_length=255)
+    document = models.CharField("CPF/CNPJ", max_length=14)
+
+    def __str__(self):
+        return self.name
+
+
+class Property(models.Model):
+    name = models.CharField("Nome", max_length=255)
+    city = models.CharField("Cidade", max_length=255)
+    state = models.CharField("Estado", max_length=255)
+    total_area = models.DecimalField("Área Total", max_digits=12, decimal_places=2)
+    arable_area = models.DecimalField("Área Agricultável", max_digits=12, decimal_places=2)
+    vegetation_area = models.DecimalField("Área de Vegetação", max_digits=12, decimal_places=2)
+    farmer = models.ForeignKey("Farmer", on_delete=models.PROTECT, verbose_name="Fazendeiro")
+
+    def __str__(self):
+        return f"Propriedade {self.name}"
+
+
+class Harvest(models.Model):
+    year = models.SmallIntegerField("Ano")
+
+    def __str__(self):
+        return f"Safra {self.year}"
+
+
+class CultivatedCrop(models.Model):
+    name = models.CharField("Nome", max_length=255)
+    harvest = models.ForeignKey("Harvest", on_delete=models.CASCADE, verbose_name="Safra")
+
+    def __str__(self):
+        return f"{self.name} na {self.harvest}"
+
+
+class CropInProperty(models.Model):
+    cultivated_crop = models.ForeignKey("CultivatedCrop", on_delete=models.CASCADE, verbose_name="Cultura")
+    property = models.ForeignKey("Property", on_delete=models.CASCADE, verbose_name="Propriedade")
+
+    def __str__(self):
+        return f"{self.cultivated_crop} na {self.property}"
---
+++ src/agriculture_api/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+
+from agriculture_api.views import (
+    FarmerViewSet, PropertyViewSet, HarvestViewSet, CultivatedCropViewSet,
+    CropInPropertyViewSet
+)
+
+router = DefaultRouter()
+router.register('farmers', FarmerViewSet)
+router.register('properties', PropertyViewSet)
+router.register('harvests', HarvestViewSet)
+router.register('cultivated-crops', CultivatedCropViewSet)
+router.register('crops-in-properties', CropInPropertyViewSet)
