f9f7053881c3e0ab74fa4d964c5883af6421d701
Jailton Silva <jss1@ifal.edu.br>
2025-06-16 18:37:48 -0300

    feat: registering models in django admin

--- src/agriculture_api/admin.py
+++ src/agriculture_api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from agriculture_api.models import (
+    Property, Farmer, CropInProperty, Harvest,
+    CultivatedCrop
+)
+
+
+@admin.register(Farmer)
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ("name", "document",)
+    search_fields = ("name", "document",)
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("name", "farmer", "city", "state", "vegetation_area", "arable_area", "total_area",)
+    search_fields = ("name", "farmer__name", "city", "state",)
+
+
+@admin.register(Harvest)
+class HarvestAdmin(admin.ModelAdmin):
+    list_display = ("year",)
+    search_fields = ("year",)
+
+
+@admin.register(CultivatedCrop)
+class CultivatedCropAdmin(admin.ModelAdmin):
+    list_display = ("name", "harvest__year",)
+    search_fields = ("name", "harvest__year",)
+
+
+@admin.register(CropInProperty)
+class CropInPropertyAdmin(admin.ModelAdmin):
+    list_display = ("cultivated_crop__name", "property__name", "cultivated_crop__harvest__year",)
